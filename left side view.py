94b91a483fdd1eca7c0b4455ead8f37b4cfed51a
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-09 16:06:02 +0530

    Create left side view.py

---
+++ left side view.py
@@ -0,0 +1,12 @@
+def leftSideView(root):
+    def solve(root, lvl):
+        if root:
+            if len(res)==lvl:
+                res.append(root.val)
+            solve(root.left, lvl + 1)    
+            solve(root.right, lvl + 1)
+        return 
+
+    res = []
+    solve(root,0)
+    return res
