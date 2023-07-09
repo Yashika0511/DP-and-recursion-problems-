3cfc08504a89a9460a82aff9c0573ad635d08faf
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-09 16:08:07 +0530

    Create Maximum Level Sum of a Binary Tree.py

---
+++ Maximum Level Sum of a Binary Tree.py
@@ -0,0 +1,15 @@
+def maxLevelSum(root):
+    min_level, max_sum = 1, -float('inf')
+    q, level = [root], 1
+    while q:
+        tmp, sum_ = [], 0
+        for node in q:
+            sum_ += node.val
+            if node.left:
+                tmp.append(node.left)
+            if node.right:
+                tmp.append(node.right)
+        if sum_ > max_sum:
+            min_level, max_sum = level, sum_
+        q, level = tmp, level+1
+    return min_level
