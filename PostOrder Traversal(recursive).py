690adc25426dec3b35ee70933435b4be260891ae
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-09 15:46:10 +0530

    Update PostOrder Traversal(recursive).py

--- PostOrder Traversal(recursive).py
+++ PostOrder Traversal(recursive).py
@@ -1,1 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
+res=[]
+def postorderTraversal(root,res):
+    if root:
+        postorderTaversal(root.left)
+        postorderTraversal(root.right)
+        res.append(root.val)
+print(res)
