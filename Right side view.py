f65cb960c002036786a5c5cb58765992de7341fa
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-09 16:03:22 +0530

    Update Right side view.py

--- Right side view.py
+++ Right side view.py
@@ -1,1 +1,12 @@
+def rightSideView(root):
+    def solve(root, lvl):
+        if root:
+            if len(res)==lvl:
+                res.append(root.val)
+            solve(root.right, lvl + 1)
+            solve(root.left, lvl + 1)
+        return 
 
+    res = []
+    solve(root,0)
+    return res
