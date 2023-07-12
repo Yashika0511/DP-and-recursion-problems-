ada787714a1a8a776b0ac9ccba5d7c673df76177
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-12 10:34:18 +0530

    Create All Nodes Distance K in Binary Tree.py

---
+++ All Nodes Distance K in Binary Tree.py
@@ -0,0 +1,24 @@
+def distanceK(root, target, ):
+        def add_parent(cur, parent):
+            if cur:
+                cur.parent = parent
+                add_parent(cur.left, cur)
+                add_parent(cur.right, cur) 
+        add_parent(root, None)
+        
+        answer = []
+        visited = set()
+        def dfs(cur, distance):
+            if not cur or cur in visited:
+                return
+            visited.add(cur)
+            if distance == 0:
+                answer.append(cur.val)
+                return
+            dfs(cur.parent, distance - 1)
+            dfs(cur.left, distance - 1)
+            dfs(cur.right, distance - 1)
+            
+        dfs(target, k)
+        
+        return answer
