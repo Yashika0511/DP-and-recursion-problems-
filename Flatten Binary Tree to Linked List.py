ac7bfeb46221fc5a6a81d17d70055833a1fec5e4
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-14 13:34:01 +0530

    Update Flatten Binary Tree to Linked List.py

--- Flatten Binary Tree to Linked List.py
+++ Flatten Binary Tree to Linked List.py
@@ -1,7 +1,7 @@
 def flatten(root):
-        self.head = None
-        def revPreOrder(node):
-            if node.right: revPreOrder(node.right)
-            if node.left: revPreOrder(node.left)
-            node.left, node.right, self.head = None, self.head, node
-        if root: revPreOrder(root)
+    self.head = None
+    def revPreOrder(node):
+        if node.right: revPreOrder(node.right)
+        if node.left: revPreOrder(node.left)
+        node.left, node.right, self.head = None, self.head, node
+    if root: revPreOrder(root)
