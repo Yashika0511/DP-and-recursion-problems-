b3bf59cb95790f2d1cfffab09c5cf4d3104cf347
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-15 23:24:28 +0530

    Create Graph BFS.py

---
+++ Graph BFS.py
@@ -0,0 +1,13 @@
+import collections
+def bfs(graph, root):
+
+    visited, queue = set(), collections.deque([root])
+    visited.add(root)
+
+    while queue:
+        vertex = queue.popleft()
+        print(str(vertex))
+        for neighbour in graph[vertex]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
