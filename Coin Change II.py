7c5c62e5ee92d00c9abd7e5c8b6d891d153edc67
Yashika0511 <114306924+Yashika0511@users.noreply.github.com>
2023-07-15 23:09:01 +0530

    Create Coin Change II.py

---
+++ Coin Change II.py
@@ -0,0 +1,9 @@
+def change(amount,coins):
+        dp = [0] * (amount+1)
+        dp[0] =1
+
+        for i in range(len(coins)-1,-1,-1):
+            for j in range(1,amount+1):
+                if j - coins[i] >=0:
+                    dp[j] += dp[j-coins[i]]
+        return dp[amount] 
